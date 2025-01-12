57496ba816491b213849e55aaccbb0caf5b89be0
adetayo adewobi <adewobiadetayo1@gmail.com>
2025-01-12 02:54:30 +0100

    feat: add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,106 @@
-import React from "react";
-
+import { useState } from "react";
+import { assets } from "../assets/frontend_assets/assets";
+import { NavLink, Link } from "react-router-dom";
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={""} alt="" />
+      <img src={assets.logo} alt="" className="w-36" />
+      <ul className=" hidden sm:flex gap-5 text-sm text gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>HOME</p>
+          <hr className="h-[1.5px] w-2/4 border-none bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/Collection" className="flex flex-col items-center gap-1">
+          <p>COLLECTION</p>
+          <hr className="h-[1.5px] w-2/4 border-none bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p>ABOUT</p>
+          <hr className="h-[1.5px] w-2/4 border-none bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p>CONTACT</p>
+          <hr className="h-[1.5px] w-2/4 border-none bg-gray-700 hidden" />
+        </NavLink>
+      </ul>
+      <div className="flex items-center gap-6">
+        <img
+          src={assets.search_icon}
+          alt="search icon"
+          className="w-5 cursor-pointer"
+        />
+        <div className="group relative">
+          <img
+            src={assets.profile_icon}
+            alt=""
+            className="w-5 cursor-pointer"
+          />
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+              <p className="cursor-pointer hover:text-black">Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} alt="" className="w-5 min-w-5" />
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-[8px] text-white aspect-squar rounded-full">
+            2
+          </p>
+        </Link>
+        <img
+          onClick={() => setVisible(true)}
+          src={assets.menu_icon}
+          alt=""
+          className="w-5 cursor-pointer sm:hidden"
+        />
+      </div>
+      {/* sidebar menu for smaller screen */}
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          visible ? "w-full" : "w-0"
+        }`}
+      >
+        <div className="flex flex-col text-gray-600">
+          <div
+            onClick={() => setVisible(false)}
+            className="flex items-center gap-4 p-3 cursor-pointer"
+          >
+            <img src={assets.dropdown_icon} className="rotate-180 h-4" alt="" />
+            <p>Back</p>
+          </div>
+          <NavLink
+            className="py-2 pl-6 border"
+            onClick={() => setVisible(false)}
+            to="/"
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            className="py-2 pl-6 border"
+            onClick={() => setVisible(false)}
+            to="/collection"
+          >
+            COLLECTION
+          </NavLink>
+          <NavLink
+            className="py-2 pl-6 border"
+            onClick={() => setVisible(false)}
+            to="/about"
+          >
+            ABOUT
+          </NavLink>
+          <NavLink
+            className="py-2 pl-6 border"
+            onClick={() => setVisible(false)}
+            to="/contact"
+          >
+            CONTACT
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 };
